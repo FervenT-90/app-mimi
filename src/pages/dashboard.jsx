14b95a2f079024417616440e2d0898ebc0a33a19
Miguel Ángel Rodríguez <mige5553@gmail.com>
2020-05-13 11:39:47 +0100

    [T] Setting Up Login Route
    -index.jsx refactored to be only the login
    -dashboard.jsx setup redirect from /dashboard to login (/)

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { navigate } from 'gatsby';
+import { Router } from '@reach/router';
 import DashboardLayout from '../layouts/dashboard.jsx';
 import RouteJoin from '../components/RouteJoin.jsx';
 import RouteWorkouts from '../components/RouteWorkouts.jsx';
 import RouteProfile from '../components/RouteProfile.jsx';
 import RouteSettings from '../components/RouteSettings.jsx';
-import { Router } from '@reach/router';
 
-const Dashboard = () => {
+// eslint-disable-next-line react/prop-types
+const Dashboard = ({ location }) => {
+   useEffect(() => {
+      // eslint-disable-next-line react/prop-types
+      if (location.pathname.match(/^\/dashboard\/?$/)) {
+         navigate('/', { replace: true });
+      }
+   }, []);
    return (
       <DashboardLayout>
          <h1>DASHBOARD</h1>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
+
 import MainLayout from '../layouts/main.jsx';
 
 const Main = () => {
    return (
       <MainLayout>
-         <h1>TODO: LOGIN/REGISTRY</h1>
-         <p>If you have already an account:</p>
-         <Link to="/dashboard">Go to dashboard</Link>
+         <div>
+            <p>Login or Sign Up</p>
+            <button>Log In</button>
+         </div>
       </MainLayout>
    );
 };
