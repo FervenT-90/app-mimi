7276da6b51c45238a5b130912db18c83c3e6aae5
Miguel Ángel Rodríguez <mige5553@gmail.com>
2020-05-22 09:53:28 +0100

    [T] Style Start Screen
    - tailwind.css: Added animation to letter-spacing text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
 import React, { FC } from 'react';
 import { Link } from 'gatsby';
 import MainLayout from '../layouts/main';
-
+import startScreen from '../assets/svgs/startScreen.min.svg';
+import Header from '../components/dashboard/Header';
+import Footer from '../components/dashboard/Footer';
 const Main: FC = () => {
    return (
       <MainLayout>
-         <div>
-            <p>TODO: Step by Step to get started</p>
-            <Link to="/dashboard">Let&apos;s start!</Link>
+         <Header />
+         <div className="flex flex-col items-center justify-center h-screen -mt-24">
+            <h1 className="mb-10 text-4xl text-white animation font-primary text-mimi-shadow">
+               Bienvenido
+            </h1>
+
+            <img src={startScreen} alt="welcome svg" />
+            <Link
+               className="px-6 py-1 mt-20 font-semibold tracking-wider text-white uppercase rounded shadow bg-green-mimi focus:outline-none focus:underline"
+               to="/dashboard"
+            >
+               Let&apos;s start!
+            </Link>
          </div>
+         <Footer />
       </MainLayout>
    );
 };
